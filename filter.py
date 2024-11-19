104d2445bb898b847159f4a53eed1ea39c5ef9e6
unknown <Matteo Paz>
2024-11-18 21:57:50 -0800

    Made SORTBY optional

--- filter.py
+++ filter.py
@@ -3,7 +3,8 @@ from pandasql import sqldf
 from __init__ import QUERY, ROWLIMIT, USE_PURE_CATALOG, SORTBY
 
 catalog = pd.read_csv("varwise_cats/pure_VarWISE.csv" if USE_PURE_CATALOG else "varwise_cats/VarWISE.csv")
-catalog = catalog.sort_values(by=SORTBY[0], ascending=SORTBY[1])
+if SORTBY:
+    catalog = catalog.sort_values(by=SORTBY[0], ascending=SORTBY[1])
 
 result = sqldf(f"SELECT * FROM catalog WHERE {QUERY}", globals()).head(ROWLIMIT).set_index("cluster_id")
 
